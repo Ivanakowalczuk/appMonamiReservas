b38bd9654b7b76c03464db71a679bb86440c3a42
ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-07-18 22:35:03 -0300

    front 4

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,8 +10,10 @@ router.get('/register', (req, res) => res.render('register'))
 router.get('/reservaExitosa', (req, res) => res.render('reservaExitosa'))
 router.get('/reservarPadel', (req, res) => res.render('reservarPadel'))
 router.get('/reservarSquash', (req, res) => res.render('reservarSquash'))
+router.get('/datosReservaPadel', (req, res) => res.render('datosReservaPadel'))
+router.get('/datosReservaSquash', (req, res) => res.render('datosReservaSquash'))
+router.get('/dashboardPadel', (req, res) => res.render('dashboardPadel'))
+router.get('/dashboardCliente', (req, res) => res.render('dashboardCliente'))
 router.get('/dashboard', (req, res) => res.render('dashboard'))
 router.get('/dashboardSquash', (req, res) => res.render('dashboardSquash'))
-router.get('/dashboardPadel', (req, res) => res.render('dashboardPadel'))
-
 export default router 
